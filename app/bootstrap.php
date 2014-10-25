695b4edf86fba41212140b2311348f038da4e1ce
hrach <hrach.cz@gmail.com>
2014-10-25 12:28:13 +0200

    fixup debugger

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,6 +10,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Configurator();
 $configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->enableDebugger();
 $configurator->createRobotLoader()->addDirectory(__DIR__)->register();
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
